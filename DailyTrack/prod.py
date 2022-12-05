ab462d41feada63091693f53ef42667e0db1a7d3
Nitin <nitin8285824551@gmail.com>
2022-12-05 16:44:57 +0530

    add production settings

---
+++ DailyTrack/prod.py
@@ -0,0 +1,29 @@
+import json
+
+with open('/etc/config.json') as config_file:
+    config = json.load(config_file)
+
+
+# If using in your own project, update the project namespace below
+from DailyTrack.base import * 
+
+
+SECRET_KEY = config['SECRET_KEY']
+
+# False if not in os.environ
+DEBUG = config['DEBUG']
+
+ALLOWED_HOSTS = config['ALLOWED_HOSTS']
+
+
+# Database settings
+DATABASES = {
+    'default': {
+        'ENGINE': config['db']['ENGINE'],
+        'NAME': config['db']['NAME'],
+        'USER': config['db']['USER'],
+        'PASSWORD': config['db']['PASSWORD'],
+        'HOST': config['db']['HOST'],
+        'PORT': config['db']['PORT'],
+    }
+}
