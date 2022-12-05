3b3f8bf3abc22eecda8c980b2cd0233f6a0e1ea6
Nitin <nitin8285824551@gmail.com>
2022-12-05 16:45:57 +0530

    change prod setting

--- DailyTrack/wsgi.py
+++ DailyTrack/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DailyTrack.base')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DailyTrack.prod')
 
 application = get_wsgi_application()
